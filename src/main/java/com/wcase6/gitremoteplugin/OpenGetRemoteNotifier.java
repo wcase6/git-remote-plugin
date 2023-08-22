e63043fe2e3c809dd63cd25aaf20c55a52c5c00c
Will Case <wcase@grubhub.com>
2023-08-22 12:30:50 -0500

    Working impl

---
+++ src/main/java/com/wcase6/gitremoteplugin/OpenGetRemoteNotifier.java
@@ -0,0 +1,16 @@
+package com.wcase6.gitremoteplugin;
+
+import com.intellij.notification.NotificationGroupManager;
+import com.intellij.notification.NotificationType;
+import com.intellij.openapi.project.Project;
+
+public class OpenGetRemoteNotifier {
+
+    public static void notifyError(Project project, String content) {
+        NotificationGroupManager.getInstance()
+                .getNotificationGroup("Git Remote Group")
+                .createNotification(content, NotificationType.ERROR)
+                .notify(project);
+    }
+
+}
---
+++ src/main/java/com/wcase6/gitremoteplugin/OpenGitRemoteAction.java
@@ -0,0 +1,71 @@
+package com.wcase6.gitremoteplugin;
+
+import com.intellij.ide.BrowserUtil;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.PsiFile;
+import git4idea.GitLocalBranch;
+import git4idea.repo.GitRemote;
+import git4idea.repo.GitRepository;
+import git4idea.repo.GitRepositoryManager;
+
+import java.util.Objects;
+import java.util.Optional;
+
+public class OpenGitRemoteAction extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        if (e.getProject() == null) {
+            OpenGetRemoteNotifier.notifyError(null, "Unable to identify project for this file");
+            return;
+        }
+
+        if (e.getProject().getBasePath() == null) {
+            OpenGetRemoteNotifier.notifyError(e.getProject(), "Unable to get base path for this project");
+            return;
+        }
+
+        PsiFile file = e.getData(CommonDataKeys.PSI_FILE);
+        if (file == null || !file.isValid()) {
+            OpenGetRemoteNotifier.notifyError(e.getProject(), "The requested file is null or invalid");
+            return;
+        }
+
+        VirtualFile vFile = file.getVirtualFile();
+        GitRepositoryManager repositoryManager = GitRepositoryManager.getInstance(e.getProject());
+        Optional<GitRepository> repository = repositoryManager.getRepositories().stream().findFirst();
+
+        if (repository.isEmpty()) {
+            OpenGetRemoteNotifier.notifyError(e.getProject(), "Unable to determine git repository");
+            return;
+        }
+
+        Optional<String> url = repository.get().getRemotes().stream()
+                        .map(GitRemote::getFirstUrl)
+                        .filter(Objects::nonNull)
+                        .findFirst();
+
+        if (url.isEmpty()) {
+            OpenGetRemoteNotifier.notifyError(null, "Unable to determine remote git url");
+            return;
+        }
+
+        String httpUrl = url.get().replaceAll("git@|git://|git@ssh\\.", "https://")
+                .replaceAll("com:", "com/")
+                .replaceAll("\\.git", "");
+
+        Optional<String> hash = repository
+                .map(GitRepository::getCurrentRevision);
+        Optional<String> branch = repository
+                .map(GitRepository::getCurrentBranch)
+                .map(GitLocalBranch::getName);
+
+        String blob = hash.orElse(branch.orElse("master"));
+
+        String relativeFilePath = vFile.getPath().replace(e.getProject().getBasePath(), "");
+        BrowserUtil.browse(httpUrl + "/blob/" + blob + relativeFilePath);
+    }
+}
